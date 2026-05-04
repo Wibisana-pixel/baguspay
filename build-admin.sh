f5a18c9dd6dbf635eeecb51b3d7513b1684d337f
Wibisana-pixel <indonesia.0.merdeka@gmail.com>
2026-05-04 10:12:07 +0700

    Update build-admin.sh

--- build-admin.sh
+++ build-admin.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 set -e 
 
-echo "1. Mencari alamat server aslinya..."
-RUMAH="$(pwd)/apps/web-admin"
-echo "Alamat ketemu di: $RUMAH"
-
-echo "2. Menyuntik obat ke tsconfig..."
-sed -i "s|\"baseUrl\": \".\"|\"baseUrl\": \"$RUMAH\"|g" apps/web-admin/tsconfig.json
-sed -i "s|\"baseUrl\": \"./\"|\"baseUrl\": \"$RUMAH\"|g" apps/web-admin/tsconfig.json
-sed -i "s|\"baseUrl\":\".\"|\"baseUrl\":\"$RUMAH\"|g" apps/web-admin/tsconfig.json
-sed -i "s|\"baseUrl\":\"./\"|\"baseUrl\":\"$RUMAH\"|g" apps/web-admin/tsconfig.json
-
-echo "3. Merakit jantung utama..."
+echo "1. Merakit jantung utama..."
 pnpm --filter "@repo/*" build
 
-echo "4. Masuk ke kamar admin..."
+echo "2. Masuk ke kamar admin..."
 cd apps/web-admin
 
-echo "5. Merakit AdonisJS..."
+echo "3. Bikin peta anti-nyasar buat mesin SWC..."
+cat << 'EOF' > .swcrc
+{
+  "jsc": {
+    "baseUrl": "/app/apps/web-admin"
+  }
+}
+EOF
+
+echo "4. Merakit AdonisJS..."
 node ace build
 
-echo "6. SELESAI BOSKU! SIAP TERBANG!"
+echo "5. ALHAMDULILLAH KELAR BOSKU!"
